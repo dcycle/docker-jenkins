5679a59c077348a475c1addb7cfa364212083135
Albert Albala <albert@mediatribe.net>
2023-09-05 12:49:37 -0400

    Please squash this commit in the pull request

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -4,6 +4,12 @@
 #
 set -e
 
+# https://stackoverflow.com/questions/68992799
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6ED0E7B82643E131
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F8D2585B8783D481
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BDE6D2B9216EC7A8
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 54404762BBB6E853
 apt-get update
 apt-get -y --no-install-recommends install python3 python3-pip
 rm -rf /var/lib/apt/lists/*
