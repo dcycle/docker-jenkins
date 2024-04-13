328e53129b607a4b30b7af11962dadedca5ed202
Albert Albala <albert@mediatribe.net>
2024-04-12 22:19:24 -0400

    Add doctl to Jenkins due to https://github.com/dcycle/docker-digitalocean-php/issues/3 (#5)

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -6,4 +6,16 @@ set -e
 
 apt-get update
 apt-get -y --no-install-recommends install python3 python3-pip
+# https://docs.digitalocean.com/reference/doctl/how-to/install/
+# Can't automatically get latest version due to
+# https://stackoverflow.com/questions/78262083
+LATEST=1.105.0
+cd ~ && curl -LO "https://github.com/digitalocean/doctl/releases/download/v$LATEST/doctl-$LATEST-linux-amd64.tar.gz"
+cd ~ && ls -lah && tar xf "doctl-$LATEST-linux-amd64.tar.gz"
+mv ~/doctl /usr/local/bin
+cd ~ && rm "doctl-$LATEST-linux-amd64.tar.gz"
 rm -rf /var/lib/apt/lists/*
+
+# see if python3 and doctl are installed
+python3 --version
+doctl version
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -38,7 +38,7 @@ docker buildx use mybuilder
 docker buildx inspect --bootstrap
 docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
 
-docker buildx build -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64 --push .
-docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64 --push .
-docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64 --push .
-docker buildx build -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64 --push .
+docker buildx build -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
